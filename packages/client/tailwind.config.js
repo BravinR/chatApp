6028f314e5864c572b619d290c7c48a14a5f70d6
BravinR <bravinrutoh@gmail.com>
2022-09-20 22:06:02 -0500

    Chat app Boilerplate code.

---
+++ packages/client/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      'viridian': '#449388',
+      'swirl':'#DAD3CC',
+      'blackSqueeze':'#DDECF2',
+      'doublePearlLusta':'#FCF4CB',
+      'concrete': '#F2F2F2',
+      'tusk':'#E2F7CB',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
